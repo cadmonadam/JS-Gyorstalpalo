02be5897ba50a5b575e5f991858944aaeba45bb3
Arnold <arniwebdev@gmail.com>
2019-12-02 22:51:53 +0200

    variables structure and documentation

---
+++ 01_Variables/variables.js
@@ -0,0 +1,79 @@
+// JavaScript hozzaadasa a HTML-hez
+
+// // hogy commentelunk kodot
+
+// console.log();
+
+// typeof Operator
+
+// Pritive tipusok
+
+// number
+// boolean
+// boolean
+// null
+// undefined
+
+// Symbol es BigInt - de ezekrol nem beszelunk csak megemlitjuk oket
+
+// ----------------------------------------------------------------------------
+
+// Szamok - JS-ben csak egy Szam tipus van
+// Pozitiv szamok
+// Negativ szamok
+// Egesz szamok
+// Tizedes szamok 1.0000000000000009
+
+// Muveletek szamokkal +, -, *, /, %, **, ()
+
+// 0 es -0
+
+// NaN - not a number 0/0, NaN + ertek
+
+// Infinity (vegtelen) 1/0 -1/0
+
+// ----------------------------------------------------------------------------
+
+// Valtozok bejelntese:
+
+// let - kulcsszoval es = ertekadas operatorral
+
+// foglalt kulcsszavak es a valtozok elnevezese (camelCase not snek_case)
+
+// valtozo ertekenek modositasa
+
+// muveletek valtozokkal
+
+// unary operators / unaris operatorok  ++, -- es +=, -=, *=, /=,
+
+// const - kulcsszoval es = ertekadas operatorral
+// valtozo erteke nem modosithato
+
+// var - bejelentesi modot mar nem hasznaljuk
+
+// ----------------------------------------------------------------------------
+
+// booleans - logikai valtozok
+// true - igaz
+// false - hamis
+
+// ----------------------------------------------------------------------------
+
+// string - szoveg
+// "" vagy ''
+
+// sring muvelet: + (osszefuzes)
+
+// string indexelve van (erdekesseg) es van ey tulajdonsaguk .length
+
+// string is immutable
+
+// string - template literals - template string ` ${}` - back-tick key
+
+// ----------------------------------------------------------------------------
+
+// null es undefined - mindketto egy ertek hianyat jelzi
+
+// null - sznadekos beallitott ures ertek, mi allitjuk be
+
+// undefined - egy ures valtozo, amit bejelentettunk DE NEM rendeltunk erteket hozza (hiba)
